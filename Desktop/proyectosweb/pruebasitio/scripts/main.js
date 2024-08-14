622b90039f7bf484128bd57abd90b2e91bd8c01c
Joaquin <joaquinserra94@gmail.com>
2024-08-14 06:59:53 +0200

    aportamos css

--- Desktop/proyectosweb/pruebasitio/scripts/main.js
+++ Desktop/proyectosweb/pruebasitio/scripts/main.js
@@ -7,6 +7,32 @@ miImage.onclick = function () {
     miImage.setAttribute("src", "images/mundo.jpg");
   }
 };
+  let miBoton = document.querySelector("button");
+  let miTitulo = document.querySelector("h1");
+
+  function estableceNombreUsuario() {
+    let miNombre = prompt("Introduzca su nombre.");
+    if (!miNombre) {
+      estableceNombreUsuario();
+    } else {
+      localStorage.setItem("nombre", miNombre);
+      miTitulo.innerHTML = "El mundo en nuestras manos, " + miNombre;
+    }  
+  
+  }
+  
+  if (!localStorage.getItem("nombre")) {
+    estableceNombreUsuario();
+  } else {
+    let nombreAlmacenado = localStorage.getItem("nombre");
+    miTitulo.textContent = "El mundo en nuestras manos, " + nombreAlmacenado;
+  }
+  
+  miBoton.onclick = function () {
+    estableceNombreUsuario();
+  };
+  
+
 
 
 
